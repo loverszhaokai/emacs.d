0b302aaf150e46346e075312d2ddf6b714cf3a33
zhaokai <zhaokai@yidian-inc.com>
2016-10-29 22:27:21 +0800

    Update the company for cpp

--- lisp/init-cpp.el
+++ lisp/init-cpp.el
@@ -16,4 +16,16 @@
 
 (add-hook 'c-mode-common-hook 'hs-minor-mode) ; enable hs minor mode
 
+;; Reference: http://blog.binchen.org/posts/emacs-as-c-ide-easy-way.html
+;; Company config for cpp
+;;
+;; cd /usr/include && MAKEOBJDIRPREFIX=~/obj gtags -O && cd ~/proj1 && gtags && cd ~/proj2 && gtags
+;;
+(setenv "GTAGSLIBPATH" (concat "/usr/include"
+                               ":"
+                               (file-truename "~/workspace/baseparser/baseparser")))
+(setenv "MAKEOBJDIRPREFIX" (file-truename "~/gtags-obj/"))
+(setq company-backends '((company-dabbrev-code company-gtags)))
+
+
 (provide 'init-cpp)
