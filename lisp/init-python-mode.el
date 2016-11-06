e683247b34c1140657f5f3a2099485361b18317f
zhaokai <zhaokai@yidian-inc.com>
2016-11-06 14:48:58 +0800

    Update python config

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -8,11 +8,14 @@
 (when (maybe-require-package 'anaconda-mode)
   (after-load 'python
     (add-hook 'python-mode-hook 'anaconda-mode)
-    (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
+    (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
+    (add-hook 'python-mode-hook 'highlight-indentation-mode))
   (when (maybe-require-package 'company-anaconda)
     (after-load 'company
       (add-hook 'python-mode-hook
                 (lambda () (sanityinc/local-push-company-backend 'company-anaconda))))))
 
+(eval-after-load "company"
+  '(add-to-list 'company-backends '(company-anaconda :with company-capf)))
 
 (provide 'init-python-mode)
